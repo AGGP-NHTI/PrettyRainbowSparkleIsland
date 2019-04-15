2ea7937eba25664fa64c5efc694a934aa3b98f0d
SCDepic <djwright0817@gmail.com>
2019-04-15 11:38:57 -0400

    Added Dialouge Script

---
+++ AG131TeamProject_PuzzleGame(working title)/Assets/Scripts/Dialouge.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+
+public class Dialouge : MonoBehaviour
+{
+    public TextAsset untranslated;
+    public TextAsset translated;
+    private Story dialouge;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   public void Untranslated()
+    {
+        dialouge = new Story(untranslated.text);
+    }
+   public void Translated()
+    {
+        dialouge = new Story(translated.text);
+    }
+}
