f15d332514c9c2368078409afd9291191e29e164
SCDepic <djwright0817@gmail.com>
2019-04-21 17:04:25 -0400

    Added music fixed translator check

--- AG131TeamProject_PuzzleGame(working title)/Assets/Scripts/CharacterInteraction.cs
+++ AG131TeamProject_PuzzleGame(working title)/Assets/Scripts/CharacterInteraction.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class CharacterInteraction : MonoBehaviour
 {
+    Inventory inv;
     GameObject trnsl;
     // Start is called before the first frame update
     void Start()
     {
-        trnsl = gameObject.GetComponentInChildren<GameObject>();
+        inv = gameObject.GetComponentInChildren<Inventory>();
+        trnsl = inv.inventory[0];
     }
 
     // Update is called once per frame
