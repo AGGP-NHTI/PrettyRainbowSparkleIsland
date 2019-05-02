79d3ea113e53af78edb9deafb9f8f3f84503c0d0
krazykris02 <49251153+krazykris02@users.noreply.github.com>
2019-05-01 22:08:51 -0400

    flower lights

---
+++ AG131TeamProject_PuzzleGame(working title)/Assets/Scripts/ColliderCheck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderCheck : MonoBehaviour
+{
+    public bool ShowCollisionEvents = false;
+    // Start is called before the first frame update
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (ShowCollisionEvents)
+        {
+            Debug.Log("Player OnCollisionEnter:" + collision.gameObject.name);
+        }
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        if (ShowCollisionEvents)
+        {
+            Debug.Log("Player OnCollisionExit:" + collision.gameObject.name);
+        }
+    }
+     
+    public void OnCollisionStay(Collision collision)
+    {
+        if (ShowCollisionEvents)
+        {
+            Debug.Log("Player OnCollisionStay:" + collision.gameObject.name);
+        }
+    }
+}
+
+   
---
+++ AG131TeamProject_PuzzleGame(working title)/Assets/Scripts/LightTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightTrigger : MonoBehaviour
+{
+    public Light flowerlight;
+    // Update is called once per frame
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entered collider");
+        flowerlight.intensity = 20;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        flowerlight.intensity = 0;
+    }
+}
