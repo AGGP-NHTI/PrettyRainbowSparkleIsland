90ca2125f66a54319a13f6a73f1299b8fa41b847
clundstedt225 <37668469+clundstedt225@users.noreply.github.com>
2019-05-06 11:55:05 -0400

    light flower fixes

--- AG131TeamProject_PuzzleGame(working title)/Assets/Scripts/LightTrigger.cs
+++ AG131TeamProject_PuzzleGame(working title)/Assets/Scripts/LightTrigger.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class LightTrigger : MonoBehaviour
 {
-    public Light flowerlight;
+    public GameObject flowerlight;
     // Update is called once per frame
+
+    public void Start()
+    {
+        //flowerlight = gameObject.GetComponentInChildren<Light>();
+    }
 
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("Entered collider");
-        flowerlight.intensity = 20;
+        flowerlight.SetActive(true);
     }
     void OnTriggerExit(Collider other)
     {
-        flowerlight.intensity = 0;
+        flowerlight.SetActive(false);
     }
 }
